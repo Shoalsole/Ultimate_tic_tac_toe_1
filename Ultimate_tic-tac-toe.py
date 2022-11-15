e0eb050ddecebc701077b344fe9d9528bef9618e
Ray Wang <116653346+Shoalsole@users.noreply.github.com>
2022-11-15 17:51:24 -0500

    Update Ultimate_tic-tac-toe.py

--- Ultimate_tic-tac-toe.py
+++ Ultimate_tic-tac-toe.py
@@ -1,1 +1,60 @@
 #This is the python file for Tic-tac-toe-ultimate. Note that the regular tic-tac-toe code is on Quinn's repo. 
+
+
+# Notes from Tuesday...
+
+#gameOne = [" "]*10
+#gameTwo = [" "]*10
+#gameThree = [" "]*10
+#gameFour = [" "]*10
+#gameFive = [" "]*10
+#gameSix = [" "]*10
+#gameSeven = [" "]*10
+#gameEight = [" "]*10
+#gameNine = [" "]*10
+
+#games = [gameOne,gameTwo,gameThree,gameFour,gameFive,fiveSix,fiveSeven,gameEight,gameNine] 
+  
+  #startGame will be the choice of the game that we start in
+  #we are doing two player
+  # moveOne is the move by player one
+  # moveTwo is the move by player two
+  
+  #playerOneLetter
+  #playerTwoLetter
+  
+  #1. print the game
+  #2. print the game we're curruently playing in (RAY)
+  
+  
+  #3. swap between games/play in the proper game (XIUWEN)
+  
+  
+  #4. local win conditions (QUINN)
+  #5. global win conditions
+  
+  
+  #7. Starting the game -while game is playing loop (JOHN)
+  
+  
+  
+  
+  #nextGame[moveOne] = playerOneLetter
+  #nextGame = (moveOne-1) 
+  
+  #nextGame = 5
+  #moveOne from player 
+  #play moveOne in nextGame - games[nextGame-1][moveOne] = playerOneLetter 
+  #change game - nextGame
+  
+  #moveTwo from player2
+  #play moveTwo in nextGame
+  #change game - reassign nextGame
+  
+  #moveOne from player 1
+  #play moveOne in nextGame
+  #ghange game - reassign nextGame
+  
+  
+  #isWinner(games[4],playerOneLetter)
+ 

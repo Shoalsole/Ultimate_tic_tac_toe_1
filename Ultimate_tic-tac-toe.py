f20fea144c19282c3ceb4be5354f4aaeb103a6e9
Ray Wang <116653346+Shoalsole@users.noreply.github.com>
2022-11-22 20:22:47 -0500

    Update Ultimate_tic-tac-toe.py

--- Ultimate_tic-tac-toe.py
+++ Ultimate_tic-tac-toe.py
@@ -66,22 +66,56 @@ import random
   
 print("Welcome to ultimate tic-tac-toe!" + "\n")
 
+print("The game is in 9 games of tic-tac toe. Of which, each tic-tac toe game is represented in one of the nine squares below. Within a tic-tac-toe game, the regular rules apply for tic-tac-toe.")
+
+print('\n' + 'The games in the top row correspond to the numbers 7, 8, 9 respectively, same goes for the middle row, where the games correspond to the numbers 4, 5, 6. Same goes for the bottom row.")
+      
+print('\n" + "Within each instance of a tic-tac-toe game, the same rules of tic-tac-toe apply.")
+      
+print("The 3 entries in the top row correspond to the number 7, 8, 9. 
+      
+print("The 3 entries in the middle row correspond to the numbers 4, 5, 6, and same logic for the bottom 3 entries - very similar to the logic with each game as described above.") 
+
 #print('-------------------------')
 #print('| . . . | . . . | . . . |')
-# print('| . . . | . . . | . . . |')
-# print('| . . . | . . . | . . . |')
-# print('-------------------------')
-# print('| . . . | . . . | . . . |')
-# print('| . . . | . . . | . . . |')
-# print('| . . . | . . . | . . . |')
-# print('-------------------------')
-# print('| . . . | . . . | . . . |')
-# print('| . . . | . . . | . . . |')
-# print('| . . . | . . . | . . . |')
-# print('-------------------------')
+#print('| . . . | . . . | . . . |')
+#print('| . . . | . . . | . . . |')
+#print('-------------------------')
+#print('| . . . | . . . | . . . |')
+#print('| . . . | . . . | . . . |')
+#print('| . . . | . . . | . . . |')
+#print('-------------------------')
+#print('| . . . | . . . | . . . |')
+#print('| . . . | . . . | . . . |')
+#print('| . . . | . . . | . . . |')
+#print('-------------------------')
+      
 
-print(gameSeven[7] + '|' + gameSeven[8] + '|' + gameSeven[9])
+      
 
+def print_board(): #maybe prints the board if the condition of the game being started is true, elsewise do not print. 
+      
+  print('----------------------------')
+
+  print('|' + gameSeven[7] + gameSeven[8] + gameSeven[9] + '|' + gameEight[7] + gameEight[8] + gameEight[9] + '|' + gameNine[7] + gameNine[8] + gameNine[9] + '|')
+  print('|' + gameSeven[4] + gameSeven[5] + gameSeven[6] + '|' + gameEight[4] + gameEight[5] + gameEight[6] + '|' + gameNine[4] + gameNine[5] + gameNine[6] + '|')
+  print('|' + gameSeven[1] + gameSeven[2] + gameSeven[3] + '|' + gameEight[1] + gameEight[2] + gameEight[3] + '|' + gameNine[1] + gameNine[2] + gameNine[3] + '|')
+
+  print('----------------------------')
+
+  print('|' + gameFour[7] + gameFour[8] + gameFour[9] + '|' + gameFive[7] + gameFive[8] + gameFive[9] + '|' + gameSix[7] + gameSix[8] + gameSix[9] + '|')
+  print('|' + gameFour[4] + gameFour[5] + gameFour[6] + '|' + gameFive[4] + gameFive[5] + gameFive[6] + '|' + gameSix[4] + gameSix[5] + gameSix[6] + '|')
+  print('|' + gameFour[1] + gameFour[2] + gameFour[3] + '|' + gameFive[1] + gameFive[2] + gameFive[3] + '|' + gameSix[1] + gameSix[2] + gameSix[3] + '|')
+
+  print('----------------------------')
+
+  print('|' + gameOne[7] + gameOne[8] + gameOne[9] + '|' + gameTwo[7] + gameTwo[8] + gameTwo[9] + '|' + gameThree[7] + gameThree[8] + gameThree[9] + '|')
+  print('|' + gameOne[4] + gameOne[5] + gameOne[6] + '|' + gameTwo[4] + gameTwo[5] + gameTwo[6] + '|' + gameThree[4] + gameThree[5] + gameThree[6] + '|')
+  print('|' + gameOne[1] + gameOne[2] + gameOne[3] + '|' + gameTwo[1] + gameTwo[2] + gameTwo[3] + '|' + gameThree[1] + gameThree[2] + gameThree[3] + '|')
+
+  print('----------------------------')
+
+      
 #Prints visual representation of global board...
 
 #Top row: 7, 8, 9
